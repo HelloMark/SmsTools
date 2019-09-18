be2b8f687acb87bd3635b279fb0c416a4cf49954
wenhe.ma <wenhe.ma@sihuatech.com>
2019-09-18 18:33:54 +0800

    receiver sms on manifest

---
+++ app/src/main/java/com/chinaboy/smstools/IncomingBroadcastReceiver.java
@@ -0,0 +1,33 @@
+package com.chinaboy.smstools;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class IncomingBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        final Bundle bundle = intent.getExtras();
+        try {
+            if (null == bundle)
+                return;
+            final Object[] objects = (Object[]) bundle.get("pdus");
+            for (int i = 0, size = objects.length; i < size; i++) {
+                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) objects[i]);
+                String phoneNumber = smsMessage.getDisplayOriginatingAddress();
+                String body = smsMessage.getMessageBody();
+
+            //    gateway.message(phoneNumber, body);
+
+                Log.i("receive", phoneNumber + " " + body);
+            }
+        } catch (Exception e) {
+            Log.e("Receive", e.getMessage());
+        }
+    }
+}
